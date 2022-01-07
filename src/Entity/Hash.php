121d0af3bab03d4125abf382dcbfde164c64a8ef
Vinicius Costa <viniciusath@hotmail.com>
2022-01-07 01:01:04 -0300

    Parte 1 do desafio completa

---
+++ src/Entity/Hash.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HashRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: HashRepository::class)]
+class Hash
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'datetime')]
+    private $Batch;
+
+    #[ORM\Column(type: 'integer')]
+    private $block;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $string;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $key;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $hash;
+
+    #[ORM\Column(type: 'integer')]
+    private $attempts;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBatch(): ?\DateTimeInterface
+    {
+        return $this->Batch;
+    }
+
+    public function setBatch(\DateTimeInterface $Batch): self
+    {
+        $this->Batch = $Batch;
+
+        return $this;
+    }
+
+    public function getBlock(): ?int
+    {
+        return $this->block;
+    }
+
+    public function setBlock(int $block): self
+    {
+        $this->bloco = $block;
+
+        return $this;
+    }
+
+    public function getString(): ?string
+    {
+        return $this->string;
+    }
+
+    public function setString(string $string): self
+    {
+        $this->string = $string;
+
+        return $this;
+    }
+
+    public function getKey(): ?string
+    {
+        return $this->key;
+    }
+
+    public function setKey(string $key): self
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+    public function getHash(): ?string
+    {
+        return $this->hash;
+    }
+
+    public function setHash(string $hash): self
+    {
+        $this->hash = $hash;
+
+        return $this;
+    }
+
+    public function getAttempts(): ?int
+    {
+        return $this->attempts;
+    }
+
+    public function setAttempts(int $attempts): self
+    {
+        $this->attempts = $attempts;
+
+        return $this;
+    }
+}
